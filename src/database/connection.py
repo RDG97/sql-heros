0f822cfb48e48bd2b8eb64c93317957afd9d1765
RDG97 <ryangann1997@gmail.com>
2022-11-03 15:20:07 +0000

    3 more functions made. starting to come together

--- src/database/connection.py
+++ src/database/connection.py
@@ -32,7 +32,16 @@ def execute_query(query, params=None):
 # create_connection('postgres', 'postgres', 'postgres')
 
 # ====================================== EDIT BELOW HERE
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def select_all():
     query = """
@@ -40,11 +49,11 @@ def select_all():
     """
 
     list_of_heroes = execute_query(query).fetchall()
-    print(list_of_heroes)
+    
     for record in list_of_heroes:
         print(record[1])
 
-# select_all()
+
 
 def create_new_hero():
     newName = input('New hero name: ')
@@ -56,7 +65,7 @@ def create_new_hero():
     query = """
         INSERT INTO heroes (name, about_me, biography)
         VALUES (%s, %s, %s);
-    """
+            """
     try: 
         execute_query(query, (newName, newAbout, newBio))
         print(f"Hero {newName} was born!")
@@ -64,8 +73,9 @@ def create_new_hero():
         print('Sorry that didnt work')
         
 
-
-create_new_hero()
+#select_all()
+#create_new_hero()
+#kill_hero()
 
 def kill_hero():
     dyingStar = input('who should die?: ')
@@ -76,4 +86,77 @@ def kill_hero():
     """
     kill_hero = execute_query(query, (dyingStar,)).fetchall()
 
-# kill_hero()
+
+def list_friendships():
+    query = """
+            SELECT * FROM heroes, relationships
+            WHERE heroes.id = relationships.hero1_id               
+            """
+    buddybuddy = execute_query(query).fetchall()
+    print(buddybuddy[1][1])
+    for x in range(len(buddybuddy)):
+        if buddybuddy[x][8] == 1:
+            relStat = f"{bcolors.OKCYAN} friends{bcolors.ENDC}"
+        else:
+            relStat = f"{bcolors.FAIL}enemies{bcolors.ENDC}"
+        hero1Name = str(buddybuddy[x][1])
+        hero2id = int(buddybuddy[x][7])
+        hero2Name = str(buddybuddy[hero2id][1])
+        print(hero1Name, f"{bcolors.WARNING} and{bcolors.ENDC}", hero2Name, f"{bcolors.WARNING} and{bcolors.ENDC}", relStat)
+
+#list_friendships()
+
+def make_friends():
+    query = """
+            SELECT * FROM heroes
+            """
+    heroes = execute_query(query).fetchall()
+    for record in heroes:
+        print(record[1], 'id:', record[0])
+    print(f"{bcolors.WARNING} here is a list of heroes and their ids{bcolors.ENDC}")
+    firsthero = input(f"{bcolors.WARNING}enter the first heroes ID: {bcolors.ENDC}")
+    secondhero = input(f"{bcolors.WARNING}now enter the second heroes id: {bcolors.ENDC}")
+    stat = input(f"{bcolors.WARNING}now how do they feel about each other? (1 for friends 2 for enemies): {bcolors.ENDC}")
+
+    query = """
+        INSERT INTO relationships (hero1_id, hero2_id, relationship_type_id)
+        VALUES (%s, %s, %s);
+            """
+    execute_query(query, (firsthero, secondhero, stat))
+    print(f"{bcolors.OKGREEN}Success! do {bcolors.ENDC}{bcolors.OKBLUE}'list_friendships'{bcolors.ENDC}{bcolors.OKGREEN} to see the new connection{bcolors.ENDC}")
+
+
+
+
+def king_prompt():
+    whatdo = input(f"{bcolors.WARNING}What would you like to do? (type 'help' for options): ")
+    if whatdo == 'list_friendships':
+        list_friendships()
+        king_prompt()
+    elif whatdo == 'kill_hero':
+        kill_hero()
+        king_prompt()
+    elif whatdo == 'create_new_hero':
+        create_new_hero()
+        king_prompt()
+    elif whatdo == 'help':
+        mls = """function option(s):
+            list_friendships(),
+            kill_hero(),
+            create_new_hero(),
+            make_friends(),
+            stop
+            """
+        print(mls)
+        king_prompt()
+    elif whatdo == 'make_friends':
+        make_friends()
+        king_prompt()
+    elif whatdo == 'stop':
+        print(f"{bcolors.OKGREEN}STOPPED{bcolors.ENDC}")
+    else:
+        print(f"{bcolors.FAIL}oops i dont think you typed what you meant to type. type 'help' for options{bcolors.ENDC}")
+        king_prompt()
+
+king_prompt()
+
